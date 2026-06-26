8d316750369e0b93693c1e88ea1fc85282ecfe3e
Bhawishya Kumar Sooryavanshi <sooryavanshibhawishya@gmail.com>
2026-06-27 02:01:56 +0530

    Create main.tsx

---
+++ apps/web/src/main.tsx
@@ -0,0 +1,16 @@
+import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+import { app } from "./firebase/firebaseConfig";
+
+console.log("Firebase Initialized:", app.name);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
